e5d4f59b2cb4a78e07b1f8aa622353204b01b4f0
ryanztr <ryanzhangtianran@gmail.com>
2025-12-08 22:56:36 +0800

    Update covt_dino model

---
+++ train/src/covt_dino.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import os
+
+from PIL import Image
+from transformers import (
+    PaliGemmaForConditionalGeneration,
+    PaliGemmaProcessor,
+    Dinov2Model,
+    AutoImageProcessor
+)
+
+PALIGEMMA_MODEL_PATH = ""
+DINO_MODEL_PATH = ""
+NUM_VIS_TOKENS = 4
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DTYPE = torch.bfloat16 if DEVICE == "cuda" else torch.float32
+print(f"Running on {DEVICE}")
+
+class COVT_DINO(nn.Module):
+    def __init__(self, PALIGEMMA_MODEL_PATH, DINO_MODEL_PATH, num_vis_tokens=4):
+        super().__init__()
+        # Initialize PaliGemma3B model
+        print(f"Loading PaliGemma from: {PALIGEMMA_MODEL_PATH}...")
+        self.paligemma = PaliGemmaForConditionalGeneration.from_pretrained(
+            PALIGEMMA_MODEL_PATH,
+            torch_dtype=DTYPE
+        )
+        # Initialize DINO model
+        print(f"Loading DINO from: {DINO_MODEL_PATH}...")
+        self.dino = Dinov2Model.from_pretrained(
+            DINO_MODEL_PATH,
+            torch_dtype=DTYPE
+        )
+
+        # Freeze PaliGemma and DINO parameters
+        for param in self.paligemma.parameters():
+            param.requires_grad = False
+        for param in self.dino.parameters():
+            param.requires_grad = False
+
+        # Dimension config
+        self.paligemma_dim = self.paligemma.config.hidden_size
+        self.dino_dim = self.dino.config.hidden_size
+
+        # Visual tokens config
+        self.num_vis_tokens = num_vis_tokens
+        self.num_patches = 256  # (224x224 image / 14 patch_size)
+
+        # Projection layer
+        self.input_projection = nn.Linear(self.paligemma_dim, self.paligemma_dim, dtype=DTYPE)
+        # Learnable queries
+        self.learnable_queries = nn.Parameter(torch.randn(1, self.num_patches, self.paligemma_dim, dtype=DTYPE))
+        # Cross-attention
+        self.cross_attention = nn.MultiheadAttention(
+            embed_dim=self.paligemma_dim, 
+            num_heads=8, 
+            batch_first=True,
+            dtype=DTYPE
+        )
+        # Output projection
+        self.output_projection = nn.Linear(self.paligemma_dim, self.dino_dim, dtype=DTYPE)
+
+    def resize_token_embeddings(self, visual_tokens_size):
+        self.paligemma.resize_token_embeddings(visual_tokens_size)
+
+    def grab_dino_features(self, pixel_values):
+        with torch.no_grad():
+            dino_outputs = self.dino(pixel_values=pixel_values, output_hidden_states=True)
+            dino_features = dino_outputs.last_hidden_state
+            return dino_features[:, -1:, :]
+
+    def forward(self, input_ids, pixel_values_paligemma, pixel_values_dino, attention_mask):
+        # Get DINO features
+        target_features = self.grab_dino_features(pixel_values_dino) # [B, 256, 768]
+
+        # Forward
+        outputs = self.paligemma(
+            input_ids=input_ids,
+            pixel_values=pixel_values_paligemma,
+            attention_mask=attention_mask,
+            output_hidden_states=True
+        )
+
+        last_hidden_state = outputs.last_hidden_state[-1]
+        visual_tokens = last_hidden_state[:, -self.num_vis_tokens:, :] # [B, 4, Dim]
+        
+        kv = self.input_projection(visual_tokens) # [B, 4, Dim]
+        batch_size = kv.shape[0]
+        q = self.learnable_queries.expand(batch_size, -1, -1) # [B, 256, Dim]
+
+        attn_output, _ = self.cross_attention(
+            query=q,
+            key=kv,
+            value=kv
+        )
+        predicted_dino_features = self.output_projection(attn_output) # [B, 256, Dim]
+        loss = nn.functional.mse_loss(predicted_dino_features, target_features)
+
+        return loss, predicted_dino_features
